4b7b7fb9b702d103958aa94ebf28217b051a50b3
Devin Zhou <devinzhou101@gmail.com>
2021-05-04 01:56:56 -0400

    finished

---
+++ index.js
@@ -0,0 +1,27 @@
+// navbar
+window.addEventListener("scroll", function () {
+  var nav = document.querySelector(".nav-container");
+  nav.classList.toggle("navscroll", window.scrollY > 0);
+});
+
+// description
+var example = [
+  "Software Development.",
+  "Data Science.",
+  "Stock Trading.",
+  "Web Development.",
+];
+
+rotate(0);
+function rotate(i) {
+  setTimeout(function () {
+    var text = document.getElementById("sequence");
+    text.innerHTML = example[i];
+
+    rotate(++i);
+
+    if (example.length == i) {
+      rotate(0);
+    }
+  }, 1500);
+}
